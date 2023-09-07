cfb96d1db43c49f75725aeb1cfb5243fdf9a2d4f
developer-jyyun <developer.jyyun@gmail.com>
2023-09-07 11:55:15 +0900

    Feat: 현재위치 기반 날씨 가져오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude; //위도
+      let lon = position.coords.longitude; //경도
+      console.log("현재위치:", lat, lon); //현재위치: 37.6471552 127.0284288
+      getWeatherByCurrentLocation(lat, lon); //현재위치: 37.6471552 127.0284288
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=83d39c03bab22f218e4d0ca7e1634476`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  return <div className="App">ggggggggggggggg</div>;
 }
 
 export default App;
